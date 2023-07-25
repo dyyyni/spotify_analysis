17ac7b4dbd0c783033f2a3ca48d3419c820423e1
Daniel Luoma <luomadaniel95@gmail.com>
2023-07-25 17:57:00 +0300

    Heatmapping : data_by_year Data by year now works. Also made the main.py compatible with it.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from scripts import data_raw_to_interim
 from scripts import data_interim_to_overview
+from scripts import data_by_year_month
 
 def main():
     data_raw_to_interim.main()
     data_interim_to_overview.main()
+    data_by_year_month.main()
--- scripts/data_by_year_month.py
+++ scripts/data_by_year_month.py
@@ -2,95 +2,96 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def main():
+    df = pd.read_json("./data/interim/interim_data.json")
 
-df = pd.read_json("../data/interim/interim_data.json")
+    df = df[df['timestamp'] != 0]
 
-df = df[df['timestamp'] != 0]
+    df['minutes_played'] = (df['ms_played'] / 1000 / 60).astype(int)
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-df['minutes_played'] = (df['ms_played'] / 1000 / 60).astype(int)
-df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df['year'] = df['timestamp'].dt.year
+    df['month'] = df['timestamp'].dt.month
 
-df['year'] = df['timestamp'].dt.year
-df['month'] = df['timestamp'].dt.month
+    df = df[df['year'] >= 2020]
 
-df = df[df['year'] >= 2020]
-
-df_music = df[df['podcast'] == False]
-df_podcast = df[df['podcast'] == True]
+    df_music = df[df['podcast'] == False]
+    df_podcast = df[df['podcast'] == True]
 
 # Create a color palette
-cmap = sns.color_palette("Reds", as_cmap=True) 
+    cmap = sns.color_palette("Reds", as_cmap=True) 
 
 # Total Listening by Year and Month
-heatmap_data_total = pd.pivot_table(
-    df,
-    values='minutes_played',
-    index='year',
-    columns='month',
-    aggfunc='sum'
-).fillna(0).astype(int)
+    heatmap_data_total = pd.pivot_table(
+        df,
+        values='minutes_played',
+        index='year',
+        columns='month',
+        aggfunc='sum'
+    ).fillna(0).astype(int)
 
-plt.figure(figsize=(10, 8))
-ax = sns.heatmap(
-    heatmap_data_total,
-    cmap=cmap,
-    annot=True,
-    annot_kws={"size": 8},
-    fmt="d", linewidths=1,
-    cbar_kws={'label': 'Minutes'})
-ax.figure.axes[-1].yaxis.label.set_size(10)
-plt.title('Total Listening by Year and Month')
-plt.ylabel('Year')
-plt.xlabel('Month')
-plt.show()
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(
+        heatmap_data_total,
+        cmap=cmap,
+        annot=True,
+        annot_kws={"size": 8},
+        fmt="d", linewidths=1,
+        cbar_kws={'label': 'Minutes'})
+    ax.figure.axes[-1].yaxis.label.set_size(10)
+    plt.title('Total Listening by Year and Month')
+    plt.ylabel('Year')
+    plt.xlabel('Month')
+    plt.savefig('./reports/figures/total_by_year.png')
 
 # Music Listening by Year and Month
-heatmap_data_music = pd.pivot_table(
-    df_music,
-    values='minutes_played',
-    index='year',
-    columns='month', 
-    aggfunc='sum'
-).fillna(0).astype(int)
+    heatmap_data_music = pd.pivot_table(
+        df_music,
+        values='minutes_played',
+        index='year',
+        columns='month', 
+        aggfunc='sum'
+    ).fillna(0).astype(int)
 
-plt.figure(figsize=(10, 8))
-ax = sns.heatmap(
-    heatmap_data_music,
-    cmap=cmap,
-    annot=True,
-    annot_kws={"size": 8},
-    fmt="d",
-    linewidths=1,
-    cbar_kws={'label': 'Minutes'}
-)
-ax.figure.axes[-1].yaxis.label.set_size(10)
-plt.title('Music Listening by Year and Month')
-plt.ylabel('Year')
-plt.xlabel('Month')
-plt.show()
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(
+        heatmap_data_music,
+        cmap=cmap,
+        annot=True,
+        annot_kws={"size": 8},
+        fmt="d",
+        linewidths=1,
+        cbar_kws={'label': 'Minutes'}
+    )
+    ax.figure.axes[-1].yaxis.label.set_size(10)
+    plt.title('Music Listening by Year and Month')
+    plt.ylabel('Year')
+    plt.xlabel('Month')
+    plt.savefig('./reports/figures/music_by_year.png')
 
 # Podcast Listening by Year and Month
-heatmap_data_podcast = pd.pivot_table(
-    df_podcast,
-    values='minutes_played',
-    index='year',
-    columns='month',
-    aggfunc='sum'
-).fillna(0).astype(int)
+    heatmap_data_podcast = pd.pivot_table(
+        df_podcast,
+        values='minutes_played',
+        index='year',
+        columns='month',
+        aggfunc='sum'
+    ).fillna(0).astype(int)
 
-plt.figure(figsize=(10, 8))
-ax = sns.heatmap(
-    heatmap_data_podcast,
-    cmap=cmap,
-    annot=True,
-    annot_kws={"size": 8},
-    fmt="d",
-    linewidths=1,
-    cbar_kws={'label': 'Minutes'}
-)
-ax.figure.axes[-1].yaxis.label.set_size(10)
-plt.title('Podcast Listening by Year and Month')
-plt.ylabel('Year')
-plt.xlabel('Month')
-plt.show()
+    plt.figure(figsize=(10, 8))
+    ax = sns.heatmap(
+        heatmap_data_podcast,
+        cmap=cmap,
+        annot=True,
+        annot_kws={"size": 8},
+        fmt="d",
+        linewidths=1,
+        cbar_kws={'label': 'Minutes'}
+    )
+    ax.figure.axes[-1].yaxis.label.set_size(10)
+    plt.title('Podcast Listening by Year and Month')
+    plt.ylabel('Year')
+    plt.xlabel('Month')
+    plt.savefig('./reports/figures/podcast_by_year.png')
 
+main()
