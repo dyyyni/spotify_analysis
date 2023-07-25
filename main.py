3fe1d9d03db2c2aeb586704fa7e6131de7a9b45b
Daniel Luoma <luomadaniel95@gmail.com>
2023-07-25 18:20:55 +0300

    Heatmapping : by_weekday Now works. main.py update to be compatible.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from scripts import data_raw_to_interim
 from scripts import data_interim_to_overview
 from scripts import data_by_year_month
+from scripts import data_by_weekday
 
 def main():
     data_raw_to_interim.main()
     data_interim_to_overview.main()
     data_by_year_month.main()
+    data_by_weekday.main()
--- scripts/data_by_weekday.py
+++ scripts/data_by_weekday.py
@@ -3,38 +3,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import calendar
 
-df = pd.read_json("../data/interim/interim_data.json")
+def main():
+    df = pd.read_json("./data/interim/interim_data.json")
 
-# First, convert the timestamp to datetime
-df['datetime'] = pd.to_datetime(df['timestamp'], unit='ms')
+    # First, convert the timestamp to datetime
+    df.loc[:, 'datetime'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-# Convert the 'ms_played' to minutes
-df['minutes_played'] = df['ms_played'] / 60000
+    # Convert the 'ms_played' to minutes
+    df.loc[:, 'minutes_played'] = df['ms_played'] / 60000
 
-# Then, apply the filter
-df = df.query('timestamp > 0 and datetime.dt.year >= 2020')
+    # Then, apply the filter
+    df = df.query('timestamp > 0 and datetime.dt.year >= 2020').copy()
 
-# Define day_dict
-day_dict = {i: day for i, day in enumerate(calendar.day_name)}
+    # Define day_dict
+    day_dict = {i: day for i, day in enumerate(calendar.day_name)}
 
-df['weekday'] = df['datetime'].dt.dayofweek
-df['weekday'] = df['weekday'].map(day_dict)
+    df.loc[:, 'weekday'] = df['datetime'].dt.dayofweek
+    df.loc[:, 'weekday'] = df['weekday'].map(day_dict)
 
-# Define the order of days in a week
-day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    # Define the order of days in a week
+    day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-# Set 'weekday' column to categorical data type and specify the order
-df['weekday'] = pd.Categorical(df['weekday'], categories=day_order, ordered=True)
+    # Set 'weekday' column to categorical data type and specify the order
+    df.loc[:, 'weekday'] = pd.Categorical(df['weekday'], categories=day_order, ordered=True)
 
-# Aggregate the data by weekday
-heatmap_data_total_weekly = df.groupby('weekday')['minutes_played'].sum()
+    # Aggregate the data by weekday
+    heatmap_data_total_weekly = df.groupby('weekday')['minutes_played'].sum()
 
-# Total Listening by Weekday
-plt.figure(figsize=(10, 8))
-sns.barplot(x=heatmap_data_total_weekly.index,
-            y=heatmap_data_total_weekly.values, palette="Reds_r", order=day_order)
-plt.title('Total Listening by Weekday')
-plt.ylabel('Minutes')
-plt.xlabel('Weekday')
-plt.show()
+    # Total Listening by Weekday
+    plt.figure(figsize=(10, 8))
+    sns.barplot(x=heatmap_data_total_weekly.index,
+                y=heatmap_data_total_weekly.values,
+                palette="Reds_r",
+                order=day_order)
+    plt.title('Total Listening by Weekday')
+    plt.ylabel('Minutes')
+    plt.xlabel('Weekday')
+    plt.savefig('./reports/figures/by_weekday.png')
+
+main()
 
